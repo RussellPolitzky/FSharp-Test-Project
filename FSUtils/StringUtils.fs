96a629e521ec971da2b1c9c46f8a53833fc5f177
russell <russell.politzky@dariel.co.za>
2012-08-10 15:04:34 +0200

    Initial commit

---
+++ FSUtils/StringUtils.fs
@@ -0,0 +1,7 @@
+﻿[<AutoOpen>]
+module StringUtils
+
+/// Functional wrappers for string methods.
+let inline toString s = s.ToString()
+let inline toLowercase (s:string) = s.ToLower()
+let inline split chars (s:string) = s.Split(chars)
---
+++ FSharpLearn/TestFSharp.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpLearn
+
+open Microsoft.VisualStudio.TestTools.UnitTesting    
+open Assertions
+
+[<TestClass>]
+type Test_FSharp() =
+   
+    [<TestMethod>]        
+    member this.``should be able to convert a sequence to a string``() =
+        [1;2;3;4;5] 
+        |> Seq.toSingleSring
+        |> Equals "12345"
